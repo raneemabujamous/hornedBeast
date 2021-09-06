ee85d9646c813b641a9f4834e57a25ade017fed8
raneemabujamous <raneemabujamous1998@gmail.com>
2021-09-06 09:59:24 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { React, Component } from "react";
+import Header from "./components/Header";
+
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
@@ -6,6 +8,7 @@ class  App extends Component{
   render(){
     return(
     <>
+    <Header/>
       <Main/>
       <Footer/>
       
